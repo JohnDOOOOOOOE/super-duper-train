2a79208c9320cc3d4f6ca9d0d51300671b60c9f7
JohnDOOOOOOOE <maximegandelin@gmail.com>
2025-11-18 13:46:10 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "suivi-montres-colis-v1";
+const CACHE_NAME = "suivi-montres-colis-v3";
 const ASSETS = [
   "./",
   "./index.html",
@@ -7,14 +7,12 @@ const ASSETS = [
   "./icon-512.png"
 ];
 
-// Installation : mise en cache des fichiers essentiels
 self.addEventListener("install", (event) => {
   event.waitUntil(
     caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
   );
 });
 
-// Activation : nettoyage des anciens caches
 self.addEventListener("activate", (event) => {
   event.waitUntil(
     caches.keys().then((keys) =>
@@ -27,27 +25,22 @@ self.addEventListener("activate", (event) => {
   );
 });
 
-// Stratégie de fetch : Cache d’abord, réseau ensuite
 self.addEventListener("fetch", (event) => {
   const request = event.request;
 
-  // On n'intercepte que les GET
   if (request.method !== "GET") return;
 
   event.respondWith(
     caches.match(request).then((cached) => {
-      // Si on a la ressource en cache → renvoi
       if (cached) return cached;
 
-      // Sinon → tentative réseau
       return fetch(request)
         .then((response) => {
-          // On met en cache la ressource récupérée si possible
           const clone = response.clone();
           caches.open(CACHE_NAME).then((cache) => cache.put(request, clone));
           return response;
         })
-        .catch(() => cached); // fallback si offline
+        .catch(() => cached);
     })
   );
 });
